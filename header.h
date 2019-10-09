ba5b36d9b3a33dc94dabaeff2f5626e244584782
JakeCardillo <55145832+JakeCardillo@users.noreply.github.com>
2019-10-09 18:30:04 -0400

    CK0 tests

---
+++ header.h
@@ -0,0 +1,60 @@
+//CK0 Machine Header
+
+#include <string.h>
+
+typedef enum {TRUE, FALSE}Bool;
+
+enum Tag { IF, NUM, APP, BOOL, PRIM, KRET, KIF, KAPP, CHECKED, UNCHECKED };
+
+typedef struct {
+	enum Tag tag;
+}expr;
+
+typedef struct {
+	expr h;
+	expr* c, *t, *f;
+}Jif;
+
+typedef struct {
+	expr h;
+	int n;
+}Jnum;
+
+typedef struct {
+	expr h;
+	expr* fun, *arg1, *arg2;
+}Japp;
+
+typedef struct {
+	expr h;
+	Bool val;
+}Jbool;
+
+typedef struct {
+	expr h;
+	char* prim;
+}Jprim;
+
+typedef struct {
+	expr h;
+}Kret;
+
+typedef struct {
+	expr h;
+	expr* t, *f, *k;
+}Kif;
+
+typedef struct {
+	expr h;
+	expr* fun, *checked, *unchecked, *k;
+}Kapp;
+
+typedef struct {
+	expr h;
+	expr* next, *data;
+}Checked;
+
+typedef struct {
+	expr h;
+	expr* next, *data;
+}Unchecked;
